53fc77569bcbf809088e3be06da10b0fce8b6df3
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-06-16 00:24:15 -0700

    making change tests passing

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,10 +2,31 @@
 
 import sys
 
-def making_change(amount, denominations):
-  # Your code here
+def making_change(amount, denominations, memo = {}):
 
-  pass
+  if amount in memo:
+    if len(denominations) in memo[amount]:
+      return memo[amount][len(denominations)]
+
+  output = 0
+  max_amount = denominations[-1]
+  max_coins = int(amount / max_amount)
+
+  if len(denominations) == 1:
+    return 1
+
+  if max_coins == 0:
+    return making_change(amount, denominations[:-1])
+
+  for i in range(0, max_coins + 1):
+    sub_total = amount - i * max_amount
+    output += making_change(sub_total, denominations[:-1])
+
+  if amount not in memo:
+    memo[amount] = {}
+
+  memo[amount][len(denominations)] = output
+  return output
 
 
 if __name__ == "__main__":
