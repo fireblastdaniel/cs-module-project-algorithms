3096cf31ae4d33bff5cfc4a3605181180d308622
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-06-15 18:37:02 -0700

    sliding window max passes tests

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -5,7 +5,29 @@ Returns: a List of integers
 def sliding_window_max(nums, k):
     # Your code here
 
-    pass
+    output = [None] * (len(nums) - k + 1)
+    # for i in range(0, len(nums)):
+    #     for j in range(0, k):
+    #         if i - j >= 0 and i - j < len(output):
+    #             if output[i-j] is None:
+    #                 output[i-j] = nums[i]
+    #             elif output[i-j] < nums[i]:
+    #                 output[i-j] = nums[i]
+
+    temp = nums[0:k]
+    temp_max = max(temp)
+
+    for i in range(0, len(output)):
+        output[i] = temp_max
+        if i + k < len(nums):
+            removed = temp.pop(0)
+            temp.append(nums[i+k])
+            if nums[i+k] > temp_max:
+                temp_max = nums[i+k]
+            elif removed == temp_max:
+                temp_max = max(temp)
+
+    return output
 
 
 if __name__ == '__main__':
