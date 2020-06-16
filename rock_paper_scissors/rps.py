97bb7f12f38b7b4d75cb9eef94e1b8aa8a2f631b
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-06-15 23:26:20 -0700

    rock paper scissors tests passing

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python
 
 import sys
+import math
 
 def rock_paper_scissors(n):
-  # Your code here
+  if n == 0:
+    return [[]]
+  
+  output = [[] for _ in range(3**n)]
+  rps_recursion(0, n, output)
+  return output
 
-  pass
+def rps_recursion(n, max_n, arr):
+  one_third = len(arr) // 3
+  two_thirds = 2 * one_third
+
+  for i in range(0, one_third):
+    arr[i].append('rock')
+
+  for i in range(one_third, two_thirds):
+    arr[i].append('paper')
+
+  for i in range(two_thirds, len(arr)):
+    arr[i].append('scissors')
+
+  if n < max_n - 1:
+    rps_recursion(n+1, max_n, arr[:one_third])
+    rps_recursion(n+1, max_n, arr[one_third:two_thirds])
+    rps_recursion(n+1, max_n, arr[two_thirds:])
 
 
 if __name__ == "__main__":
